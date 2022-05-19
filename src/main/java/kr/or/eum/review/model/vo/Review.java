5eb9f20142d1d085ad05dec243bcc5c727164aac
qaz <qaz@DESKTOP-8P6HN0K>
2022-05-19 18:34:05 +0900

    리뷰, 찜하기

--- src/main/java/kr/or/eum/review/model/vo/Review.java
+++ src/main/java/kr/or/eum/review/model/vo/Review.java
@@ -1,5 +1,17 @@
 package kr.or.eum.review.model.vo;
 
-public class Review {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Review {
+	private int memberNo;
+	private int productNo;
+	private String reviewNo;
+	private String reviewContent;
+	private int reviewStar;
+	
 }
---
+++ src/main/java/kr/or/eum/wishlist/model/vo/Wishlist.java
@@ -0,0 +1,15 @@
+package kr.or.eum.wishlist.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Wishlist {
+	private int wishNo;
+	private int productNo;
+	private int memberNo;
+
+}

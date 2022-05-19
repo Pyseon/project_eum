6b6a390236494b091e9abb8a5da9074e320f67d8
qaz <qaz@DESKTOP-8P6HN0K>
2022-05-19 18:58:59 +0900

    package kr.or.eum.ideamarket.model.dao /IdeamarketDao.java 축가

---
+++ src/main/java/kr/or/eum/ideamarket/model/service/IdeamarketService.java
@@ -0,0 +1,14 @@
+package kr.or.eum.ideamarket.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.eum.ideamarket.model.dao.IdeamarketDao;
+
+@Service
+public class IdeamarketService {
+	@Autowired
+	private IdeamarketDao ideamarketDao;
+		
+		
+	}

4d2a5091b92743d320326ba421331c5e9753329e
vetyverlo <89384937+vetyverlo@users.noreply.github.com>
2022-05-20 22:52:50 +0900

    header > 임시경로 설정, 추후 코드 삭제 productController > 임시경로 설정, 추후 코드 삭제 webapp/css/productDetail.css 추가 webapp/WEB-INF/views/product/productDetail.jsp 추가

--- src/main/java/kr/or/eum/product/controller/ProductController.java
+++ src/main/java/kr/or/eum/product/controller/ProductController.java
@@ -2,6 +2,7 @@ package kr.or.eum.product.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.eum.product.model.service.ProductService;
 
@@ -9,4 +10,9 @@ import kr.or.eum.product.model.service.ProductService;
 public class ProductController {
 	@Autowired
 	private ProductService productService;
+	
+	@RequestMapping(value = "/productDetail.do")
+	public String productDetail() {
+		return "product/productDetail";
+	} //jsp로 이동하기 위한 임시 비즈니스로직, 재건님이 로직짜고 삭제해주세요.
 }

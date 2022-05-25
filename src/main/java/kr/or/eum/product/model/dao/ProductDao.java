9e813f415fbff2e147943a01f34f024500c76e5e
qaz <qaz@DESKTOP-8P6HN0K>
2022-05-25 09:54:16 +0900

    product 페이징 구현

--- src/main/java/kr/or/eum/product/model/dao/ProductDao.java
+++ src/main/java/kr/or/eum/product/model/dao/ProductDao.java
@@ -1,6 +1,7 @@
 package kr.or.eum.product.model.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -15,8 +16,14 @@ public class ProductDao {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
-	public ArrayList<Product> selectProductList() {
-		List list = sqlSession.selectList("product.selectProductList");
+	public ArrayList<Product> selectProductList(HashMap<String, Object> map) {
+		List list = sqlSession.selectList("product.selectProductList", map);
 		return (ArrayList<Product>)list;
+	}
+
+	public int selectProductCount() {
+		// TODO Auto-generated method stub
+		int totalCount = sqlSession.selectOne("product.selectTotalCount");
+		return totalCount;
 	}
 }

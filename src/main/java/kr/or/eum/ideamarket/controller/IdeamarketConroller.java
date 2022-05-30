1c81de04b5497a3d54acf9113a6a14cd2d7208a9
qaz <qaz@DESKTOP-8P6HN0K>
2022-05-30 15:11:28 +0900

    productSQL 설명

--- src/main/java/kr/or/eum/ideamarket/controller/IdeamarketConroller.java
+++ src/main/java/kr/or/eum/ideamarket/controller/IdeamarketConroller.java
@@ -2,6 +2,8 @@ package kr.or.eum.ideamarket.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.eum.ideamarket.model.service.IdeamarketService;
 
@@ -9,4 +11,10 @@ import kr.or.eum.ideamarket.model.service.IdeamarketService;
 public class IdeamarketConroller {
 	@Autowired
 	private IdeamarketService ideamarketService;
+	
+	@RequestMapping(value="/ideamarketList.do")
+	public String ideamarketList() {
+		return "product/ClassList";
+	}
+	
 }

dd08b82a01822d5d0acc9cf92a1aa99d0ebb1408
Taeseon Kim <abcc@kakao.com>
2022-06-07 03:23:04 +0900

    댓글구현완료 (세션값 조정만 필요함)

--- src/main/java/kr/or/eum/community/controller/CommunityController.java
+++ src/main/java/kr/or/eum/community/controller/CommunityController.java
@@ -70,6 +70,7 @@ public class CommunityController {
 	@ResponseBody
 	@RequestMapping(value="/commCoWrite.do", produces="application/json;charset=utf-8")
 	public void communityCoWrite(CommunityCo commCo) {
+		System.out.println(commCo);
 		//insert 후 selectKey를 이용해 방금 insert 한 pk값 얻어옴
 		service.commCoWrite(commCo);
 		//얻어온 pk값으로 데이터 조회 후 리턴
